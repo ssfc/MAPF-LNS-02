09d990020d0a161edbcbc940e52754beb799acd3
SSFC <sortsimilar@gmail.com>
2025-12-21 16:23:55 +0800

    log << "seed=" << rand_seed << "\n";

--- src/LNS.cpp
+++ src/LNS.cpp
@@ -1436,7 +1436,7 @@ void LNS::write_path_for_viz(string file_name) const
     log << "sum_of_loss_lb=" << get_sum_of_costs_lower_bound(ins, dist_table)
         << "\n";
     log << "comp_time=" << comp_time_ms << "\n";
-    log << "seed=" << seed << "\n";
+    log << "seed=" << rand_seed << "\n";
     if (log_short) return;
     log << "starts=";
     for (size_t i = 0; i < ins.N; ++i) {
